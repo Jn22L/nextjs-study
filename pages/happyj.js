e68ce1f9e7c245a02e3a694e18ebf4426b3741ab
Jn22L <peaceofjn@gmail.com>
2022-08-20 21:25:30 +0900

    이미지 비율 수정

--- pages/happyj.js
+++ pages/happyj.js
@@ -20,7 +20,7 @@ export default function Home({ happyjs }) {
         .movie img {
           max-width: 100%;
           width: 400px;
-          height: 400px;
+          height: 200px;
           border-radius: 12px;
           transition: transform 0.2s ease-in-out;
           box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px;
