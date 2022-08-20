315103350fc6a5068e2c9be886819ed6e66a5b9e
Jn22L <peaceofjn@gmail.com>
2022-08-20 13:24:45 +0900

    1

--- pages/index_ssr.js
+++ pages/index_ssr.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import Seo from "../components/Seo";
 export default function Home({ results }) {
   const [movies, setMovies] = useState();
