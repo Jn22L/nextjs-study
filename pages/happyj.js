f70b1bb5212f0dd9da2bda3ef42ad9ef9a3c0efe
Jn22L <peaceofjn@gmail.com>
2022-08-20 19:10:00 +0900

    happyj img 추가

--- pages/happyj.js
+++ pages/happyj.js
@@ -19,6 +19,8 @@ export default function Home({ happyjs }) {
         }
         .movie img {
           max-width: 100%;
+          width: 400px;
+          height: 400px;
           border-radius: 12px;
           transition: transform 0.2s ease-in-out;
           box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px;
